2d33eb67f8ee4f64294ed93ad6f86d4cce13a62e
Steve Houston <steven.houston@airship.co.uk>
2015-06-22 14:57:19 +0100

    small changes

--- tests/LoggerTest.php
+++ tests/LoggerTest.php
@@ -6,8 +6,8 @@ class LoggerTest extends PHPUnit_Framework_TestCase {
 	public function __construct(){
 		parent::__construct();
 
-		$this->mockLogger = $this->getMockBuilder('airshipwebservices\webmonitorclient\Logger')
-						 ->setMethods(array('makeWebMonitorRequest'))
+		$this->mockLogger = $this->getMockBuilder( 'airshipwebservices\webmonitorclient\Logger' )
+						 ->setMethods( array('makeWebMonitorRequest') )
 						 ->getMock();
 
 		$this->params = [
@@ -26,7 +26,7 @@ class LoggerTest extends PHPUnit_Framework_TestCase {
 		$fail_return_obj->body = ['body'];
 		
 		$this->mockLogger->expects( $this->once() )
-				 		 ->method('makeWebMonitorRequest')
+				 		 ->method( 'makeWebMonitorRequest' )
 				 		 ->will( $this->returnValue($fail_return_obj) );
 
 		$this->assertEquals( True, $this->mockLogger->writeLog( $this->params ) );
@@ -38,7 +38,7 @@ class LoggerTest extends PHPUnit_Framework_TestCase {
 		$fail_return_obj->body = ['body'];
 
 		$this->mockLogger->expects( $this->once() )
-				 		 ->method('makeWebMonitorRequest')
+				 		 ->method( 'makeWebMonitorRequest' )
 				 		 ->will( $this->returnValue($fail_return_obj) );
 
 		$this->assertEquals( False, $this->mockLogger->writeLog( $this->params ) );
@@ -49,7 +49,7 @@ class LoggerTest extends PHPUnit_Framework_TestCase {
 		$fail_return_obj->code = 200;
 
 		$this->mockLogger->expects( $this->once() )
-				 		 ->method('makeWebMonitorRequest')
+				 		 ->method( 'makeWebMonitorRequest' )
 				 		 ->will( $this->returnValue($fail_return_obj) );
 
 		$this->assertEquals( False, $this->mockLogger->writeLog( $this->params ) );
