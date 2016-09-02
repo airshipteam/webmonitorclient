0886132a7a1692761dfbd004d926f04ab1246c94
Chris Endcliffe <chris.endcliffe@airship.co.uk>
2016-09-02 09:55:48 +0100

    Airbrake logging added in

--- app/Lib/Airbrake.php
+++ app/Lib/Airbrake.php
@@ -5,6 +5,7 @@ namespace App\Lib;
 
 
 use Airbrake\Notifier;
+use App\Models\Log;
 use App\Models\SelfLog;
 use App\Models\WebApp;
 
@@ -17,22 +18,38 @@ class Airbrake
       $app = WebApp::find($input['web_app_id']);
 
       if(!$app){
+          AirbrakeLogger::error('Coldn\'t find app', $input );
+
           return false;
       }
 
 
+      $airbrakeCon = new Notifier(['projectId' => '126972', 'projectKey' => '1028a99cb409ee35ab22e53966e457c']);
 
-      $airbrakeCon = new Notifier(['projectId' => '126972', 'projectKey' => '1028a99cb409ee35ab22e53966e457c8']);
-
-
-      $response = $airbrakeCon->sendNotice([
+    try{
+        $response = $airbrakeCon->sendNotice([
             "errors" => [[
                 "type" =>  $app->app_name . ' - Error: ' . $input['msg'],
                 "message" => $app->app_name . ' - Error: ' . $input['msg']
-                ]
+            ]
             ],
-      ]);
+        ]);
 
+    }catch(\Exception $e)
+    {
+        AirbrakeLogger::error($e->getMessage(), $input);
+
+        return false;
+    }
+
+      if(isset($response['error']))
+      {
+          AirbrakeLogger::error($response['error'], $input);
+
+          return false;
+      }
+
+      AirbrakeLogger::debug('Error sent', $input);
 
       return true;
 
